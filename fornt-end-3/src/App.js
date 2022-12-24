4f212a2ad212cd4b6b465dd01add8ef006c78ae5
Bagher hosseini <bagherhosseini03@gmail.com>
2022-12-24 20:31:42 +0100

    changed file name

--- fornt-end-3/src/App.js
+++ fornt-end-3/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Ok from './pages/ok.js';
+import Test from './pages/test.js';
 import Home from './pages/Home.js';
 import Product from './pages/product.js';
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
@@ -11,13 +11,13 @@ function App() {
       <header>
         <Link to="/Home">home</Link>
         <Link to="/product">Products</Link>
-        <Link to="/ok">ok</Link>
+        <Link to="/test">ok</Link>
       </header>
       <main>
         <Routes>
           <Route path="/Home" element={<Home />} />
           <Route path="/product" element={<Product />} />
-          <Route path="/ok" element={<Ok />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </main>
       
