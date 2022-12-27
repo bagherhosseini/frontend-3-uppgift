e050a593365da810c9603025542bf18de6d2ccd6
Jiro <jirawan.sangthong@yh.nackademin.se>
2022-12-27 19:33:44 +0100

    new delete button & filter data

--- fornt-end-3/src/pages/product.js
+++ fornt-end-3/src/pages/product.js
@@ -1,40 +1,59 @@
-import { products } from './../data/db.json';
-import React, { useState, useEffect } from 'react';
-import './style.scss';
+import { products } from "./../data/db.json";
+import React, { useState, useEffect } from "react";
+import "./style.scss";
 
-export default function Product(){
-    const [data, setData] = useState(null);
-    const [data1, setData1] = useState();
+export default function Product() {
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-      async function fetchData() {
-        const response = await fetch('http://localhost:3001/products');
-        const json = await response.json();
-        setData(json);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3001/products");
+      const json = await response.json();
+      setData(json);
+    }
+    fetchData();
+  }, []);
+
+  async function handleDelete(event) {
+    event.preventDefault();
+    const id = Number(event.target.name);
+    try {
+      const response = await fetch(`http://localhost:3001/products/${id}`, {
+        method: "DELETE",
+      });
+      const result = await response.json();
+      if (result.success) {
+        const index = data.findIndex((item) => item.id === id);
+        data.splice(index, 1);
+        setData([...data]);
       }
-      fetchData();
-    }, []); 
-  
-    return (
-      <div className='product-container'>
-        <h3>Alla produkter</h3>
-        {data ? (
-          <ul className='products'>
-            {data && data.map(item => (
-              <li className='product' key={item.id}>
-                <div className='product-content'>
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="product-container">
+      <h3>Alla produkter</h3>
+      {data ? (
+        <ul className="products">
+          {data &&
+            data.map((item) => (
+              <li className="product" key={item.id}>
+                <div className="product-content">
                   <img src={item.imageUrl}></img>
-                  <p className='productname'>{item.name}</p>
+                  <p className="productname">{item.name}</p>
                   <p>{item.price} SEK</p>
-                  <button>Tabort</button>
+                  <button onClick={handleDelete} name={item.id}>
+                    Radera
+                  </button>
                 </div>
               </li>
             ))}
-          </ul>
-        ) : (
-          <p>Loading data...</p>
-        )}
-      </div>
-    );
+        </ul>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
 }
-
--- fornt-end-3/src/pages/test.js
+++ fornt-end-3/src/pages/test.js
@@ -1,5 +1,24 @@
-export default function Test(){
+import _ from "lodash";
+import { useEffect, useState } from "react";
+
+export default function Test() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3001/products");
+      const json = await response.json();
+      setData(json);
+    }
+    fetchData();
+  }, []);
+
+  const activeData = _.filter(data, { name: "BOULOGNE BAG" });
+
   return (
-    <h1>Upload Image</h1>
-  )
+    <div>
+      {activeData.map((item) => (
+        <div key={item.id}>{item.price}</div>
+      ))}
+    </div>
+  );
 }
